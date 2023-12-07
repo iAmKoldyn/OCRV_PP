186e7d991651816e8ea3819bcc6c8693a1b30b4f
iAmKoldyn <pataraya951@gmail.com>
2023-12-07 14:33:07 +0300

    underline

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from torchvision.transforms import functional as F
 
+
 class FootballFieldDataset(Dataset):
     def __init__(self, image_dir, mask_dir, transform=None):
         self.image_dir = image_dir
@@ -18,8 +19,10 @@ class FootballFieldDataset(Dataset):
         self.transform = transform
         self.images = os.listdir(image_dir)
 
+
     def __len__(self):
         return len(self.images)
+
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.image_dir, self.images[idx])
@@ -47,14 +50,6 @@ class FootballFieldDataset(Dataset):
 
         return image, mask
 
-def get_model(num_classes=3):
-    model = smp.Unet(
-        encoder_name="resnet34", 
-        encoder_weights="imagenet", 
-        in_channels=3, 
-        classes=num_classes
-    )
-    return model
 
 def train_one_epoch(model, dataloader, optimizer, criterion):
     model.train()
@@ -67,6 +62,19 @@ def train_one_epoch(model, dataloader, optimizer, criterion):
         optimizer.step()
         total_loss += loss.item()
     return total_loss / len(dataloader)
+
+
+def get_model(num_classes=4):
+    # model = smp.PSPNet(
+    # model = smp.DeepLabV3(
+    model = smp.Unet(
+        encoder_name="resnet34", 
+        encoder_weights="imagenet", 
+        in_channels=3, 
+        classes=num_classes)
+
+    return model
+
 
 def validate(model, dataloader, criterion):
     model.eval()
@@ -103,37 +111,50 @@ def dice_score(output, target):
     return dice.item()
 
 
+def post_process_mask(mask):
+    mask = mask.astype(np.uint8)
+
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=2)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel, iterations=2)
+    return closing
+
 def visualize(model, data_loader, num_images=5):
     model.eval()
     images, _ = next(iter(data_loader))
     with torch.no_grad():
         preds = model(images)
-    preds = torch.argmax(preds, dim=1)
+    preds = torch.argmax(preds, dim=1).cpu().numpy()
     images = images.cpu().numpy()
-    preds = preds.cpu().numpy()
-    fig, ax = plt.subplots(nrows=num_images, ncols=2, figsize=(10, num_images * 5))
+
+    num_images = min(num_images, len(images))
+    fig, ax = plt.subplots(nrows=num_images, ncols=3, figsize=(15, num_images * 5))
     for i in range(num_images):
+        processed_mask = post_process_mask(preds[i])
         ax[i, 0].imshow(np.transpose(images[i], (1, 2, 0)))
         ax[i, 1].imshow(preds[i], cmap='gray')
+        ax[i, 2].imshow(processed_mask, cmap='gray')
         ax[i, 0].set_title("Original Image")
         ax[i, 1].set_title("Predicted Mask")
+        ax[i, 2].set_title("Post Processed Mask")
         ax[i, 0].axis('off')
         ax[i, 1].axis('off')
+        ax[i, 2].axis('off')
     plt.show()
+
 
 def main():
     dataset_root = 'segmentation_labeled_dataset'
     transform = transforms.Compose([
-    transforms.Resize((256, 256)), 
-    transforms.ToTensor()
-])
+        transforms.Resize((256, 256)), 
+        transforms.ToTensor()
+    ])
 
     train_dataset = FootballFieldDataset(
         image_dir=os.path.join(dataset_root, 'train', 'JPEGImages'),
         mask_dir=os.path.join(dataset_root, 'train', 'SegmentationClass'),
         transform=transform
     )
-
     val_dataset = FootballFieldDataset(
         image_dir=os.path.join(dataset_root, 'val', 'JPEGImages'),
         mask_dir=os.path.join(dataset_root, 'val', 'SegmentationClass'),
@@ -143,30 +164,38 @@ def main():
     train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
 
-    model = get_model(num_classes=3)
+    model = get_model(num_classes=4)
     criterion = nn.CrossEntropyLoss()
+    # optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    # optimizer = optim.Adam(model.parameters(), lr=1e-4)
     optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    # optimizer = optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
+    # optimizer = optim.RMSprop(model.parameters(), lr=1e-4)
 
+
+    best_val_loss = float('inf')
+    best_model_path = ""
     num_epochs = 25
+
     for epoch in range(num_epochs):
         train_loss = train_one_epoch(model, train_loader, optimizer, criterion)
-        val_loss = 0
-        iou_total = 0
-        dice_total = 0
-        count = 0
-        for images, masks in val_loader:
-            outputs = model(images)
-            loss = criterion(outputs, masks)
-            val_loss += loss.item()
-            iou_total += iou_score(outputs, masks)
-            dice_total += dice_score(outputs, masks)
-            count += 1
-        val_loss /= count
-        avg_iou = iou_total / count
-        avg_dice = dice_total / count
-        print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss}, Val Loss: {val_loss}, IoU: {avg_iou}, Dice: {avg_dice}")
+        val_loss = validate(model, val_loader, criterion)
+        print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss}, Val Loss: {val_loss}")
 
-    visualize(model, val_loader)
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_model_path = f'model_checkpoint_epoch_{epoch}.pth'
+            torch.save(model.state_dict(), best_model_path)
+            print(f"Checkpoint saved for epoch {epoch} with validation loss: {val_loss}")
+
+    print("Training complete.")
+
+    if best_model_path:
+        model.load_state_dict(torch.load(best_model_path))
+
+    visualize_results = True
+    if visualize_results:
+        visualize(model, val_loader)
 
 if __name__ == "__main__":
     main()
